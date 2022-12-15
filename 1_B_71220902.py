f7785a638d3c1b11a5a7fbbe22fb408a73ed548c
rizkychristiaan <117886340+rizkychristiaan@users.noreply.github.com>
2022-12-15 12:58:46 +0700

    Add files via upload

---
+++ 1_B_71220902.py
@@ -0,0 +1,47 @@
+import turtle 
+s = turtle.Screen()
+
+t = turtle.Turtle()
+
+
+#darwing r902c
+
+t.left(90)
+t.forward(120)
+t.right(90)
+t.forward(70)
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(70)
+t.right(225)
+t.forward(110)
+t.penup()
+t.left(45) 
+t.forward(45)
+
+t.left(90)
+t.forward(100)
+t.pendown()
+t.circle(-30,540)
+t.forward(80)
+t.circle(-30,180)
+t.circle(-120,10 )
+t.penup()
+t.right(65)
+t.forward(180)
+t.pendown() 
+t.circle(120)
+t.forward(50)
+
+
+
+
+
+
+
+
+
+
+s.exitonclick()
+
